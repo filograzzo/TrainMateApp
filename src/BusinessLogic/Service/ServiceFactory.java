68b8efab172089f349b13389b91ac5391e96180e
nayla <nayla.kouider@edu.unifi.it>
2024-09-15 16:28:15 +0200

    Aggiunta della servicefactory e servicemanager

--- src/BusinessLogic/Service/ServiceFactory.java
+++ src/BusinessLogic/Service/ServiceFactory.java
@@ -1,4 +1,44 @@
 package BusinessLogic.Service;
 
 public class ServiceFactory {
+    private static ServiceFactory instance;
+    public final int USER_SERVICE = 1;
+    public final int PROFILEPT_SERVICE = 2;
+    public final int PERSONALTRAINERHOME_SERVICE = 3;
+    public final int AGENDA_SERVICE = 4;
+    public final int WORKOUT_SERVICE = 5;
+    public final int PROFILE_SERVICE = 6;
+    public final int CUSTOMERHOME_SERVICE = 7;
+    public final int BOOKCOURSE_SERVICE = 8;
+    public final int BOOKAPPOINTMENT_SERVICE = 9;
+    public static ServiceFactory getInstance() {
+        if (instance == null)
+            instance = new ServiceFactory();
+        return instance;
+    }
+    public Object getService(int serviceType) {
+        switch (serviceType) {
+            case USER_SERVICE:
+                return ServiceManager.getInstance().getUserService();
+            case PROFILEPT_SERVICE:
+                return ServiceManager.getInstance().getProfilePTService();
+            case PERSONALTRAINERHOME_SERVICE:
+                return ServiceManager.getInstance().getPersonalTrainerHomeService();
+            case AGENDA_SERVICE:
+                return ServiceManager.getInstance().getAgendaService();
+            case WORKOUT_SERVICE:
+                return ServiceManager.getInstance().getWorkoutService();
+            case PROFILE_SERVICE:
+                return ServiceManager.getInstance().getProfileService();
+            case CUSTOMERHOME_SERVICE:
+                return ServiceManager.getInstance().getCustomerHomeService();
+            case BOOKCOURSE_SERVICE:
+                return ServiceManager.getInstance().getBookCourseService();
+            case BOOKAPPOINTMENT_SERVICE:
+                return ServiceManager.getInstance().getBookAppointmentService();
+            default:
+                return null;
+        }
+    }
+
 }
--- src/BusinessLogic/Service/ServiceManager.java
+++ src/BusinessLogic/Service/ServiceManager.java
@@ -19,22 +19,55 @@ public class ServiceManager {
     private PersonalTrainerHomeService personalTrainerHomeService;
     private AgendaService agendaService;
     private ProfilePTService profilePTService;
-
+    private UserService userService;
+    private BaseUser user;
     public UserService getUserService() {
         return userService;
     }
+    public AgendaService getAgendaService() {
+        return agendaService;
+    }
+    public ProfilePTService getProfilePTService() {
+        return profilePTService;
+    }
+    public PersonalTrainerHomeService getPersonalTrainerHomeService() {
+        return personalTrainerHomeService;
+    }
+    public CustomerHomeService getCustomerHomeService() {
+        return customerHomeService;
+    }
+    public BookAppointmentService getBookAppointmentService() {
+        return bookAppointmentService;
+    }
+    public WorkoutService getWorkoutService() {
+        return workoutService;
+    }
 
-    private UserService userService;
-    private BaseUser user;
+    public BookCourseService getBookCourseService() {
+        return bookCourseService;
+    }
+    public ProfileService getProfileService() {
+        return profileService;
+    }
+
     private ServiceManager()  {
         try {
             connection = DatabaseUtil.getConnection();
         } catch (SQLException e) {
             throw new RuntimeException("Error while connecting to the database");
         }
+        //creare e assegnare i DAO ai vari service
         CustomerDAO customerDAO = new CustomerDAO(connection);
         PersonalTrainerDAO personalTrainerDAO = new PersonalTrainerDAO(connection);
+        ExerciseDetailDAO excerciseDetailDAO = new ExerciseDetailDAO(connection);
+        MachineDAO machineDAO = new MachineDAO(connection);
+        ScheduleDAO scheduleDAO = new ScheduleDAO(connection);
+        SignedDAO signedDAO = new SignedDAO(connection);
+        TrainingDAO trainingDAO = new TrainingDAO(connection);
+
+
         userService = new UserService(customerDAO, personalTrainerDAO);
+        //#TODO:costruire i vari service e assegnare qui i loro DAO
     }
 
     public static ServiceManager getInstance() {
